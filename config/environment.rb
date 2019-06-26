6b38750db71d3730817c36117bafe88fcd85473c
James Britton <brittonjp@gmail.com>
2019-06-26 15:34:52 +0000

    rewrote basic structure to match template from Michael

---
+++ config/environment.rb
@@ -0,0 +1,11 @@
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+module Cli_Project
+  class Error < StandardError; end
+    #Your code goes here...
+  end
+  
+require_relative '../lib/cli'
+#require_relative '../lib/movie'
+require_relative '../lib/scraper'
