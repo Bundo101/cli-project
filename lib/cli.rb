d4d6ed28e23b0b36a455b5096f6802fe5b028c54
James Britton <brittonjp@gmail.com>
2019-07-03 16:02:50 +0000

    improved formatting on get_input text

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,18 +1,21 @@
-require_relative './scraper.rb'
-require_relative './movie.rb'
+# require_relative './scraper.rb'
+# require_relative './movie.rb'
 
 class CLI
   
   def welcome
     puts "Greetings discerning movie patron! Welcome to the Hollywood Walk of Shame!"
-    puts "\nThe 75 worst motion pictures to ever disappoint an audience and violently offend critics have been ranked according to the magnitude of their failure."
+    puts "\nThe 75 worst motion pictures to ever disappoint an audience and violently" 
+    puts "offend critics have been ranked according to the magnitude of their failure."
   end
   
   def get_input
     puts "\nYou have the following options:"
-    puts "  If you would like more information on a specific rank please enter that rank as a number (eg for the worst movie enter 1 or for the 20th worst movie enter 20)."
-    puts "  Or if you would like to see a random movie from this list please enter \"random\""
-    puts "  And finally if you would like to view the entire list please enter \"all\"" 
+    puts "\nIf you would like more information on a specific rank please enter that" 
+    puts "rank as a number from 1 to 75 (eg for the worst movie type 1 and press enter)."
+    puts "\nIf you would like to see a random movie from this list please type \"random\""
+    puts "\nIf you would like to view the entire list please type \"all\"" 
+    puts "\nTo close the program please type \"exit\"" 
     raw_input = gets.chomp
     input = raw_input.to_i
     if (1..75).include?(input)
@@ -21,7 +24,7 @@ class CLI
     elsif raw_input.downcase == "random"
       puts Movie.all.sample.title                                     #Need to add and format movie info output here
     elsif raw_input.downcase == "all"
-      puts Movie.all.each { |movie| puts movie.title }                #Need to add and format movie info output here
+      Movie.all.each { |movie| puts movie.title }                #Need to add and format movie info output here
     else
       puts "Please enter valid input"
     end
